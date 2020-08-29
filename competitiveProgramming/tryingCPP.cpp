0400e164a513ae7855a056effc0d71bf311a66f8
pankaj <pankaj@localhost.localdomain>
2020-08-29 13:41:11 +0530

    made graph as refresher

--- competitiveProgramming/tryingCPP.cpp
+++ competitiveProgramming/tryingCPP.cpp
@@ -1,6 +1,47 @@
-#include <iostream>
+/******************************************
+* AUTHOR : Pankaj Kumar *
+* NICK : pankaj2kumar *
+* INSTITUTION : Internet University *
+******************************************/
+#include <bits/stdc++.h>
 using namespace std;
+#define LL long long
+#define N 100005
+#define MOD 1000000007
+#define dd double
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define REP(i,a,b) for(int i=a;i<b;i++)
+#define rep1(i,b) for(int i=1;i<=b;i++)
+#define pb push_back
+#define mp make_pair
+#define clr(x) x.clear()
+#define sz(x) ((int)(x).size())
+#define F first
+#define S second
 
-int main(){
-    cout<<"Whoai";
+void solve(){
+	cout<<"buugghy started !!";
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+	//ios_base& scientific (ios_base& str);
+	#ifndef ONLINE_JUDGE 
+		freopen("input.txt", "r", stdin); 
+		freopen("error.txt", "w", stderr); 
+		freopen("output.txt", "w", stdout); 
+	#endif 
+
+	int t=1; 
+	cin>>t; 
+	while(t--) 
+	{ 
+	  solve(); 
+	  cout<<"\n"; 
+	} 
+
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
+
+
+	return 0;
 }
